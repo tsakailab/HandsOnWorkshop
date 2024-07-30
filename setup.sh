e2783cd2956cb46f229a9b0b2bd92787048d61b5
Takeda Keita <102941251+TKeitacw@users.noreply.github.com>
2024-07-30 11:47:58 +0900

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install build-essential libffi-dev libssl-dev zlib1g-dev liblzma-dev libbz2-dev libreadline-dev libsqlite3-dev libopencv-dev tk-dev libmpv-dev libmpv2
+sudo ln -s /usr/lib/x86_64-linux-gnu/libmpv.so /usr/lib/libmpv.so.1
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo '' >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+source ~/.bashrc
+pyenv install 3.11
+pyenv global 3.11
